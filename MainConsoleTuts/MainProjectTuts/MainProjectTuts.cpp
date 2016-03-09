53a95931c4ebbcdf014fe24f291da6ef8c5260f6
Ace0fspad3s <ayedawut@gmail.com>
2016-03-09 02:12:16 -0500

    Finished tutorials up til video 6.

--- MainConsoleTuts/MainProjectTuts/MainProjectTuts.cpp
+++ MainConsoleTuts/MainProjectTuts/MainProjectTuts.cpp
@@ -1,11 +1,14 @@
 // MainProjectTuts.cpp : Defines the entry point for the console application.
-//
+// Do Tutorial 6
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+
     return 0;
 }
 
